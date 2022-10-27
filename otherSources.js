b223ec1a2c6394922a51eb6a22fa2226f234db27
Heather Dice <heatherdice22@gmail.com>
2022-10-27 19:51:29 -0400

    Alternate, cleaner answer to first algo

--- otherSources.js
+++ otherSources.js
@@ -1,7 +1,6 @@
 //How would you return the vowels in a string?
 function findVowels(str) {
     let lowerString = str.toLowerCase();
-    console.log(lowerString);
     let vowels = [];
     for(let i = 0; i < lowerString.length; i++) {
         let letter = lowerString[i];
@@ -11,3 +10,26 @@ function findVowels(str) {
     }
     return vowels;
 };
+//OR use .includes(), for-of
+const vowels = ['a','e','i','o','u']
+function findVowels2(str) {
+    let lowerString = str.toLowerCase();
+    let stringVowels = [];
+    for(i of lowerString) {
+        if(vowels.includes(i)) {
+            stringVowels.push(i);
+        }
+    }
+    return stringVowels;
+};
+
+/* Password detection system detects password as *similar* if num of vowels = num of consonants.
+Passwords consist of lowercase English characters only. Vowels are a, e, i, o, u. Password length is even.
+To check strength & hackability, some manipulations can be made to password.
+In one operation, any char of the str can either be incremented or decremented.
+Ex: f can be incremented to g or decremented to e.
+a cannot be decremented and z cannot be incremented.
+Find minimum num of operations in which password can be made *similar*.
+Ex: consider password = "hack." h can be changed to i in 1 operation. resultant str is "iack"
+which has 2 vowels (i & a) & 2 consonants (c & k). Hence, str is similar.
+Minimum num of operations required to make str similar is 1. Return 1. */
