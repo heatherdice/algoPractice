a7f888b84db0420c67517c01949883a8f6fdba6e
Heather Dice <heatherdice22@gmail.com>
2022-11-01 10:50:36 -0400

    Solved Factorial algorithm problem

--- dojoFundamentals/dojoFundamentals5.js
+++ dojoFundamentals/dojoFundamentals5.js
@@ -10,3 +10,16 @@ function sigma(num) {
     }
     return sum;
 };
+
+/* Factorial
+Write a function factorial(num) that, given a number, returns
+the product of all positive integers from 1 up to number (inclusive).
+Ex: factorial(3) = 6 (or 1*2*3). factorial(5) = 120 (or 1*2*3*4*5) */
+function factorial(num) {
+    let product = 1;
+    while(num > 0) {
+        product *= num;
+        num--;
+    }
+    return product;
+};
