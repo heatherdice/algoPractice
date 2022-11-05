bd8793fb3f18682eaa2c17d17abfc785a06dd735
Heather Dice <heatherdice22@gmail.com>
2022-11-05 13:19:22 -0400

    Solved fourth "challenge" algo

--- dojoChallenges.js
+++ dojoChallenges.js
@@ -28,4 +28,12 @@ function sumOddNums() {
     }
     return sum;
 };
-console.log(sumOddNums());
+
+// Write a function that returns the sum of all values within an array
+function sumArrNums(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+};
