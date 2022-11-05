aac0c03f701eb3558976f7838ce8284d9ea3e00f
Heather Dice <heatherdice22@gmail.com>
2022-11-05 13:17:09 -0400

    Solved third "challenge" algo

--- dojoChallenges.js
+++ dojoChallenges.js
@@ -8,7 +8,7 @@ function numList() {
 };
 
 // Write a function that would get the sum of all even numbers from 1 to 1000
-function sumNums() {
+function sumEvenNums() {
     let sum = 0;
     for(let i = 0; i < 1001; i++) {
         if(i %2 === 0) {
@@ -17,3 +17,15 @@ function sumNums() {
     }
     return sum;
 };
+
+// Write a function that returns the sum of all odd numbers from 1 to 5000
+function sumOddNums() {
+    let sum = 0;
+    for(let i = 0; i < 5001; i++) {
+        if(i %2 !== 0) {
+            sum += i;
+        }
+    }
+    return sum;
+};
+console.log(sumOddNums());
