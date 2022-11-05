d91e0189bdc8370324ea9da1a9dad388cf7a2c41
Heather Dice <heatherdice22@gmail.com>
2022-11-05 13:22:48 -0400

    Solved fifth "challenge" algo

--- dojoChallenges.js
+++ dojoChallenges.js
@@ -37,3 +37,14 @@ function sumArrNums(arr) {
     }
     return sum;
 };
+
+// Given an arr w/ multiple vals, write a function that returns the max num in the arr
+function maxVal(arr) {
+    let max = arr[0];
+    for(let i = 1; i < arr.length; i++) {
+        if(max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    return max;
+};
