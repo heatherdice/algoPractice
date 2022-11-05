6504b7e43273a4417a43b7742fdf8108af4181fc
Heather Dice <heatherdice22@gmail.com>
2022-11-05 13:00:49 -0400

    Solved first "challenge" algo

---
+++ dojoChallenges.js
@@ -0,0 +1,9 @@
+// Write a function that returns an array with all numbers between 1 & 255
+function numList() {
+    let arr = [];
+    for(let x = 1; x < 256; x++) {
+        arr.push(x);
+    }
+    return arr;
+};
+
