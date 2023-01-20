64dd1daea573ab394a593020ed44c0abec0fef4c
Heather Dice <heatherdice22@gmail.com>
2023-01-20 11:57:40 -0500

    Solved Values Greater than Second in Python

--- dojoFundamentals/dojoFund2.py
+++ dojoFundamentals/dojoFund2.py
@@ -8,15 +8,13 @@ def countdown(int):
         arr.append(int)
         int -= 1
     return arr
-print(countdown(5))
 
 # Print and Return
 # Your function will receive an array with two numbers.
 # Print the first value, and return the second.
-def printAndReturn(arr):
+def prntAndReturn(arr): # because 'print' is a function in Python already, must abbreviate to avoid errors
     print(arr[0])
     return arr[1]
-print(printAndReturn([9,13]))
 
 # First Plus Length
 # Given an array, return the sum of the first value in the array,
@@ -24,6 +22,19 @@ print(printAndReturn([9,13]))
 # but a string or a boolean?
 def firstPlusLength(arr):
     return arr[0] + len(arr)
-print(firstPlusLength([0,1,2,3,4,5]))
-print(firstPlusLength(["happy",9,10])) # error: cannot concatenate str to int
-print(firstPlusLength([True,5,9])) # True = 1, so 1+3=4
+# if arr[0] == str, error: cannot concatenate str to int
+# if arr[0] == boolean, True = 1; False = 0
+
+# Values Greater than Second
+# For [1,3,5,7,9,13], print values that are greater than its 2nd value.
+# Return how many values this is.
+oddArr = [1,3,5,7,9,13]
+def greaterThanSecond(oddArr):
+    result = 0
+    for x in oddArr:
+        if x > oddArr[1]:
+            print(x)
+            x += 1
+            result += 1
+    return result
+print(greaterThanSecond(oddArr))
