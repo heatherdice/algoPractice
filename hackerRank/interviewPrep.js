ed56ff5edd83651d419fb8fd2377ca348c7f7b18
Heather Dice <heatherdice22@gmail.com>
2023-01-29 10:15:03 -0500

    Solved Jumping on the Clouds

--- hackerRank/interviewPrep.js
+++ hackerRank/interviewPrep.js
@@ -22,3 +22,27 @@ function countingValleys(steps, path) { // steps = int, path = str
     }
     return valleys;
 }
+
+/* Jumping on the Clouds
+Game w/ consecutively numbered clouds: thunderheads (1) & cumulus (0). Player can jump on any cumulus (0) w/ 
+num = cumulus + 1 || num = cumulus + 2. Player must avoid thunderheads (1). Determine min num of jumps it will take
+to jump from start to last cloud. Always possible to win. Arr of clouds for each game numbered 0 if safe, 1 if not. */
+function jumpingOnClouds(c) { // c = arr of binary ints
+    // set placeholder variables for min num of jumps & index
+    let minJumps = 0;
+    let i = 0;
+    // loop through arr until getting to second-to-last index; avoids index out of range err
+    while(i < c.length-1) {
+        // 2 jumps (jump over 1 or jump over consecutive 0)
+        if(c[i+1] >= 0 && i + 2 < c.length && c[i+2] == 0) {
+            minJumps++;
+            i+=2; // increase index by 2 to skip
+        }
+        // 1 jump (jump to next consecutive 0)
+        else if(c[i+1] == 0) {
+            minJumps++;
+            i++; // increment i to avoid endless loop
+        }
+    }
+    return minJumps;
+}
