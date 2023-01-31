fa8860a9dbbfeba4b9ff08d431220472ba21de4d
Heather Dice <heatherdice22@gmail.com>
2023-01-31 18:41:45 -0500

    Solved Simple Array Sum

--- hackerRank/hackerRank.py
+++ hackerRank/hackerRank.py
@@ -3,3 +3,13 @@
 # Ex: a = 7, b = 3; return 10 */
 def solveMeFirst(a,b):
     return a+b
+
+# Simple Array Sum
+# Given an arr of ints, find sum of its elements
+# Ex: [1,2,3] -> 1+2+3=6 -> return 6
+def simpleArraySum(arr):
+    sum = 0
+    for i in range(len(arr)):
+        sum += arr[i]
+    return sum
+
