dd2a0c3f7e9f12edf0c37f836ad4c31cba590731
Heather Dice <heatherdice22@gmail.com>
2023-02-01 10:39:27 -0500

    Solved Day 1

---
+++ hackerRank/30daysOfCode.py
@@ -0,0 +1,7 @@
+# Hello World
+# Read a full line of input from stdin & save it to dynamically typed var.
+# Print string literal saying "Hello World" to stdout.
+# Write line of code that prints content of dynamically typed var to stdout.
+input_string = input('Welcome to 30 Days of Code!')
+print('Hello, World.')
+print(input_string)
