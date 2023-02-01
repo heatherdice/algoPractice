1e65148254ad4ee8723f93eb423c01dc3a0412fd
Heather Dice <heatherdice22@gmail.com>
2023-02-01 09:44:18 -0500

    Solved Data Types

--- hackerRank/tenDaysOfJS.js
+++ hackerRank/tenDaysOfJS.js
@@ -3,3 +3,16 @@ Write a function that takes a string, parameterVariable. Print that string to th
 function greeting(parameterVariable) {
     console.log(parameterVariable);
 }
+
+/* Data Types
+Use the + operator to: convert secondInteger to int, then sum w/ firstInteger & print result,
+convert secondDecimal to float, then sum w/ firstDecimal & print result,
+print concatenation of firstString & secondString. */
+function performOperation(secondInteger,secondDecimal,secondString) {
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstString = 'HackerRank';
+    console.log(firstInteger+Number(secondInteger)); // shouldn't need Number() method? but HR required it for both
+    console.log(firstDecimal+Number(secondDecimal)); // ditto above
+    console.log(firstString+' '+secondString); // hackerRank included ' ' in their secondString
+}
