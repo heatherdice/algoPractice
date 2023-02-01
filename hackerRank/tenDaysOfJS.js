9a1af6c511518c55a5c5eea4700ad1cd881e3411
Heather Dice <heatherdice22@gmail.com>
2023-02-01 10:50:26 -0500

    Solved Loops

--- hackerRank/tenDaysOfJS.js
+++ hackerRank/tenDaysOfJS.js
@@ -26,3 +26,17 @@ function getPerimeter(l,w) {
     return 2(l+w);
 }
 
+/* Loops
+Print each vowl in str "s" on a new line. Then, print each consonant in str "s" on a new line. */
+function vowelsAndConsonants(s) { // s = str
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] == "a" || s[i] == "e" || s[i] == "i" || s[i] == "o" || s[i] == "u") {
+            console.log(s[i]);
+        }
+    }
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] != "a" && s[i] != "e" && s[i] != "i" && s[i] != "o" && s[i] != "u") {
+            console.log(s[i]);
+        }
+    }
+}
