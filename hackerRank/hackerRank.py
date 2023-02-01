f1364eece83402ed813b98ddd62a205df0a05602
Heather Dice <heatherdice22@gmail.com>
2023-01-31 19:01:32 -0500

    Solved Compare the Triplets

--- hackerRank/hackerRank.py
+++ hackerRank/hackerRank.py
@@ -13,3 +13,17 @@ def simpleArraySum(arr):
         sum += arr[i]
     return sum
 
+# Compare the Triplets
+# Find comparison points between triplet a & b by comparing a[0] w/ b[0],
+# a[1] w/ b[1], a[2] w/ b[2]. If a[i] > b[i], Alice gets 1 pt.
+# If a[i] < b[i], Bob gets 1 point. If a[i] = b[i], no pts awarded.
+# Return array with Alice's score first & Bob's score second.
+def compareTriplets(a,b):
+    alice = 0
+    bob = 0
+    for i in range(len(a)):
+        if a[i] > b[i]:
+            alice+=1
+        elif b[i] > a[i]:
+            bob+=1
+    return [alice,bob]
