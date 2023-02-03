781b7ed12c31fa41d5323566adb8e7ee937bc82c
Heather Dice <heatherdice22@gmail.com>
2023-02-03 14:04:12 -0500

    Solved Data Types

--- hackerRank/30daysOfCode.py
+++ hackerRank/30daysOfCode.py
@@ -5,3 +5,19 @@
 input_string = input('Welcome to 30 Days of Code!')
 print('Hello, World.')
 print(input_string)
+
+# Data Types
+# Vars i, d, & s are already declared & initialized for you. You must:
+# - declare 3 vars: one of type int, on of type double, & one of type str
+# - read 3 lines of input from stdin & initialize your 3 vars
+# - use the + operator to print sum of i+your int on a new line, print sum of d+double, concatenate s w/ your str.
+i = 4
+d = 4.0
+s = 'HackerRank'
+i2 = int(input())
+d2 = float(input())
+s2 = str(input())
+print(i+i2)
+print(d+d2)
+print(s+s2)
+
