db5d44765710087a6fac0bb450744db8b493efc6
Heather Dice <heatherdice22@gmail.com>
2023-02-07 11:44:16 -0500

    Solved centuryFromYear

--- codeSignal/codeSignal.py
+++ codeSignal/codeSignal.py
@@ -2,3 +2,11 @@
 # Write a function that returns the sum of two numbers
 def solution(param1, param2):
     return param1 + param2
+
+# centuryFromYear
+# Given a year, return the century it is in. The 1st century spans from year 1 up to & including year 100, 2nd century from year 101
+# up to & including year 200, etc.
+import math
+def solution(year):
+    century = math.ceil(year/100)
+    return century
