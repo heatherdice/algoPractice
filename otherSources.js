090c6a2ecd727caeef84fe0557ccd5e0872f6c56
Heather Dice <heatherdice22@gmail.com>
2023-02-09 18:27:10 -0500

    Getting Different Number work in progress

--- otherSources.js
+++ otherSources.js
@@ -33,3 +33,22 @@ Find minimum num of operations in which password can be made *similar*.
 Ex: consider password = "hack." h can be changed to i in 1 operation. resultant str is "iack"
 which has 2 vowels (i & a) & 2 consonants (c & k). Hence, str is similar.
 Minimum num of operations required to make str similar is 1. Return 1. */
+
+
+/* Getting a Different Number
+Given an arr of unique nonnegative ints, implement function that finds the smallest nonnegative int
+that is NOT in the arr. First solve without modifying arr. Then try for solution that does modify arr.
+Example: input arr = [0,1,2,3], output = 4 */
+function getDifferentNumber(arr) {
+    let x = 0;
+    for(let i = 0; i < arr.length + 1; i++) {
+        if(arr[i] >= x) {
+            x++;
+        }
+        else{
+            return x;
+        }
+    }
+}
+console.log(getDifferentNumber([0,1,2,3]));
+//SOLUTION WORKS FOR ARRAYS IN ASCENDING ORDER ONLY. NEED TO FIGURE OUT NON-ORDERED ARRAYS
