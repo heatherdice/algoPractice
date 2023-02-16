743bcabc8b563f8b797d1ef895fb208f232bcce4
Heather Dice <heatherdice22@gmail.com>
2023-02-16 16:40:29 -0500

    Began work on Poor Kenny

--- dojoFundamentals/dojoFund4.py
+++ dojoFundamentals/dojoFund4.py
@@ -1,3 +1,4 @@
+import random
 # Only Keep the Last Few
 # Given arr and num X, remove all except the last X elements.
 # Return arr (changed and shorter). Ex: given ([2,4,6,8,10],3),
@@ -17,4 +18,14 @@ def mathHelp(m,b):
 # Out of the last 100 days, there were 10 days with volcanoes, 15 with tsunamis,
 # 20 earthquakes, 25 blizzards, and 30 meteors. If these probabilities continue,
 # write whatHappensToday() to print a day's outcome.
-# def whatHappensToday():
+def whatHappensToday():
+    today = random.randrange(1,100) # random num from 1-100
+    events = {
+        "volcano" : today<=10,
+        "tsunami" : today<=25,
+        "earthquake" : today<=45,
+        "blizzard" : today<=60,
+        "meteor" : today<=90
+    }
+    # now what...? compare today var to vals in dict?
+print(whatHappensToday())
